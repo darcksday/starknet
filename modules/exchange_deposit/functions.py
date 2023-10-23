90cb24d735ccac8f9d1f5ea296c7b5b7f2008cca
Vlodkow <vlodkow@gmail.com>
2023-10-23 13:25:28 +0300

    Update exchange withdraw

--- modules/exchange_deposit/functions.py
+++ modules/exchange_deposit/functions.py
@@ -2,16 +2,13 @@ from datetime import datetime
 from loguru import logger
 from common import TOKEN_ADDRESS
 from config.settings import *
-from helpers.common import int_to_wei
+from helpers.common import int_to_wei, wei_to_int
 from helpers.csv_helper import write_csv_success, start_csv
 from helpers.starknet import Starknet
 
 
 def transfer_eth(account: Starknet, recipient, amount: float):
-    logger.info(f"[{account._id}][{account.address_original}] Transfer {amount} ETH to {recipient}")
-
     balance_wei = account.account.get_balance_sync(TOKEN_ADDRESS["ETH"])
-
     if not amount:
         amount_wei = balance_wei - int_to_wei(MIN_BALANCE_ETH)
     else:
@@ -19,6 +16,12 @@ def transfer_eth(account: Starknet, recipient, amount: float):
         if amount_wei + int_to_wei(MIN_BALANCE_ETH) > balance_wei:
             logger.error(f"Insufficient funds for transfer, skip")
             return
+
+    logger.info(f"[{account._id}][{account.address_original}] Transfer {wei_to_int(amount_wei)} ETH to {recipient}")
+
+    if amount_wei < int_to_wei(0.00001):
+        logger.error(f"Too small transaction amount, skip")
+        return
 
     contract = account.get_contract(TOKEN_ADDRESS["ETH"])
     transfer_call = contract.functions["transfer"].prepare(int(recipient, 16), amount_wei)
