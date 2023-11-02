878f85aaaf14ec7d934f620ad0a831d0d38e4ce8
Vlodkow <vlodkow@gmail.com>
2023-11-02 17:30:08 +0200

    Volumes - fix amount on OKX check

--- modules/volume/functions.py
+++ modules/volume/functions.py
@@ -137,7 +137,7 @@ def run_one_wallet_volume(account: Starknet, recipient, cex_network):
         logger.info(f"[{account._id}] Check OKX main account balance")
         main_acc_balance = get_okx_token_balance(0)
 
-        if main_acc_balance >= amount:
+        if main_acc_balance >= withdraw_amount:
             logger.success(f"[{account._id}] {main_acc_balance} ETH found")
             break
         else:
@@ -146,7 +146,7 @@ def run_one_wallet_volume(account: Starknet, recipient, cex_network):
                     logger.info(f"[{account._id}] Check OKX subAccount {get_okx_sub_account_name(sub_account_num)}")
                     acc_balance = get_okx_token_balance(sub_account_num)
 
-                    if acc_balance >= amount:
+                    if acc_balance >= withdraw_amount:
                         logger.success(f"[{account._id}] {acc_balance} ETH found, transfer to OKX main account")
                         okx_account = get_okx_account()
                         okx_account.transfer("ETH", acc_balance, get_okx_sub_account_name(sub_account_num), 'master')
@@ -154,7 +154,7 @@ def run_one_wallet_volume(account: Starknet, recipient, cex_network):
                         break
                     elif acc_balance > 0:
                         logger.info(
-                            f"[{account._id}] Only {acc_balance} ETH found, waiting {amount} ETH...")
+                            f"[{account._id}] Only {acc_balance} ETH found, waiting {withdraw_amount} ETH...")
 
         sleeping(int(MIN_SLEEP / 2), int(MAX_SLEEP / 2))
         continue
